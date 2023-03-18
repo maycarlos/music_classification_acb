2f315e6c7d8acf1d24fd6cf3efc94920c44002e3
Elmer <56842806+maycarlos@users.noreply.github.com>
2023-03-18 17:18:48 +0000

    Minimum Distance Classifier contruction

--- src/models/minimum_distance.py
+++ src/models/minimum_distance.py
@@ -1,29 +1,41 @@
 import numpy as np
 from scipy.spatial.distance import cdist
-from sklearn.base import BaseEstimator
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.multiclass import unique_labels
+from sklearn.utils.validation import check_is_fitted, check_X_y
 
 
-class MDC(BaseEstimator):
+class MinimumDistanceClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self):
-        self.class_list = None
-        self.means = None
+        self.classes_ = None
+        self.__means = None
 
     def fit(self, X, y):
-        self.class_list = np.unique(y, axis=0)
+        X,y = check_X_y(X, y)
 
-        self.means = np.zeros((len(self.class_list), X.shape[1]))
+        self.classes_ = unique_labels(y)
+        self.__means = np.zeros((len(self.classes_), X.shape[1]))
 
-        for i in range(len(self.class_list)):
-            temp = np.where(y == self.class_list[i])[0]
+        for i in range(len(self.classes_)):
+            temp = np.where(y == self.classes_[i])[0]
 
-            self.means[i, :] = np.mean(X.iloc[temp, :], axis=0)
+            self.__means[i, :] = np.mean(X.iloc[temp, :], axis=0)
+
+        return self
 
     def predict(self, X):
-        temp = np.argmin(cdist(X, self.means), axis=1)
-        y_pred = np.array([self.class_list[i] for i in temp])
+        check_is_fitted(self)
+        temp = np.argmin(cdist(X, self.__means), axis=1)
+        y_pred = np.array([self.classes_[i] for i in temp])
 
         return y_pred
 
+    @property
+    def class_means(self):
+        return self.__means
+
+
+
+
 if __name__ == "__main__":
     ahah = MDC()
-
